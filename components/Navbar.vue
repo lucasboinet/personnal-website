7fd464aebcc68cef3c0e39be6e79200038e6c50a
Lucas Boinet <boinet1906@gmail.com>
2024-11-05 21:51:04 +0100

    feat: anchor links

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -3,13 +3,13 @@
     <img :src="logo" class="size-10">
 
     <ul class="hidden md:flex flex-row gap-12 list-none items-center text-white-alt">
-      <li><a href="#contact">Contact</a></li>
-      <li><a href="#expertise">Expertise</a></li>
+      <li><a href="#clients">Contact</a></li>
+      <li><a href="#contact">Expertise</a></li>
       <li><a href="#projets">Projets</a></li>
     </ul>
 
     <a href="https://calendar.google.com/calendar/u/0/appointments/schedules/AcZssZ234fMyOY_JaOSjYoFVPK8gGHv5esLaX_6TrvRf8B1bq_uDhBoy4Su1eBXMDhZcZMXenB2-_E_9" target="_blank">
-      <button type="button" class="bg-primary text-white-alt rounded-lg px-8 py-3 text-xs font-medium">Reserver un appel</button>
+      <button type="button" class="bg-primary text-white-alt rounded-lg px-8 py-3 text-xs font-medium">Planifier un appel</button>
     </a>
   </div>
 </template>
